469bbafb1376ea49c6cf3743d96cbcc1a5a11789
Braslyvm <125586293+Braslyvm@users.noreply.github.com>
2025-05-26 20:14:11 -0600

    se crean archivos cita y paciente
    se crean la clase para citas y pacientes, con algunos métodos iniciales

---
+++ Codigo/Paciente.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PY2_Espere_pronto_le_antendemos
+{
+    public class Paciente
+    {
+        private static int lastIdPaciente = 0;
+        public int IdPaciente { get; set; }
+        public string Nombre { get; set; }
+        public string Apellido { get; set; }
+
+        public List<Cita>? Citas { get; set; }
+
+        public Paciente(string nombre, string apellido)
+        {
+            lastIdPaciente++;
+            IdPaciente = lastIdPaciente;
+            Nombre = nombre;
+            Apellido = apellido;
+             public List<Cita> Citas { get; private set; } = new List<Cita>();
+    }
+}
+}
