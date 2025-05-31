70d80eb77a6ed40baea9d57aa42546c415d62867
Braslyvm <125586293+Braslyvm@users.noreply.github.com>
2025-05-31 12:00:32 -0600

    Interfaz
    se agregan la interfaz principal y respectivamente un parte de los formularios de creación

--- PY_Analisis/Controllers/GeneralController.cs
+++ PY_Analisis/Controllers/GeneralController.cs
@@ -1,40 +1,54 @@
 using Microsoft.AspNetCore.Mvc;
 using AGBACKEND; // Importa tus clases
 
-namespace PY_Analisis.Controllers
+namespace PY_Analisis.Controllers;
+
+public class GeneralController : Controller
 {
-    public class GeneralController : Controller
+    //Listas de objetos 
+    public static List<Especialidad> Especialidades = new();
+
+    //metodos 
+    public IActionResult CrearEspecialidad(string nombre, TimeOnly duracion)
     {
-        //Listas de objetos 
-        public static List<Especialidad> Especialidades = new();
 
-        //metodos 
-        public IActionResult CrearEspecialidad(string nombre, TimeOnly duracion)
-        {
-            
-            duracionTime=ObtenerSegundosTotales(duracion);
-            // Crear la especialidad
-            var nuevaEspecialidad = new Especialidad(nombre, duracionTime);
-            Especialidades.Add(nuevaEspecialidad);
-            return true;
-        }
-        
-
-
-
-        
-        // metodos auxiliares de la clase 
-         public static int ObtenerSegundosTotales(TimeOnly duracion)
-    {
-            return (int)duracion.ToTimeSpan().TotalSeconds;
+        duracionTime = ObtenerSegundosTotales(duracion);
+        // Crear la especialidad
+        var nuevaEspecialidad = new Especialidad(nombre, duracionTime);
+        Especialidades.Add(nuevaEspecialidad);
+        return true;
     }
-       
-       
-        // metodos get formularios 
 
-        public IActionResult CrearEspecialidad()
-        {
-            return View();
-        }
+
+
+
+
+
+    // metodos auxiliares de la clase 
+    public static int ObtenerSegundosTotales(TimeOnly duracion)
+    {
+        return (int)duracion.ToTimeSpan().TotalSeconds;
+    }
+
+
+    // metodos get formularios 
+
+    public IActionResult CrearEspecialidad()
+    {
+        return View();
+    }
+            public IActionResult AgregarPacientes()
+    {
+       return PartialView("AgregarPacientes");
+    }
+     public IActionResult AgregarEspecialidad()
+    {
+       return PartialView("AgregarEspecialidad");
+    }
+     public IActionResult CrearConsultorio()
+    {
+       return PartialView("CrearConsultorio");
+    }
+
     }  
-}
+    
