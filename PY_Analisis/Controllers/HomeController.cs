3c2d54623bb6f011a90ee25d30e95fc32115184b
Braslyvm <125586293+Braslyvm@users.noreply.github.com>
2025-06-01 13:00:20 -0600

    Mensaje avisos
    se agrega un estilo existente y se implementa mensaje de avisos

--- PY_Analisis/Controllers/HomeController.cs
+++ PY_Analisis/Controllers/HomeController.cs
@@ -11,27 +11,29 @@ namespace PY_Analisis.Controllers;
 public class HomeController : Controller
 {
     public static List<Paciente> ListaPaciente { get; set; } = new List<Paciente>();
-    public static List<Especialidad> ListaEspecialidad { get; set; } = new List<Especialidad>();  
+    public static List<Especialidad> ListaEspecialidad { get; set; } = new List<Especialidad>();
+    public static List<Consultorios> ListaConsultorios { get; set; } = new List<Consultorios>();
     private static bool datosCargados = false;
-      public IActionResult Index()
+    public IActionResult Index()
     {
         return View();
     }
 
     public async Task<IActionResult> Sala()
     {
-       if (!datosCargados)
-    {
-        await CargarPaciente();
-        await CargarEspecialidad();
-        datosCargados = true;
-    }
+        if (!datosCargados)
+        {
+            await CargarPaciente();
+            await CargarEspecialidad();
+            datosCargados = true;
+        }
         return View(ListaPaciente);
     }
-     
-   [HttpPost]
+
+    [HttpPost]
     public IActionResult AgregarEspecialidad(string nombre, string duracion)
-    {    if (ListaPaciente.Any(p => p.Nombre == nombre ) )
+    {
+        if (ListaPaciente.Any(p => p.Nombre == nombre))
         {
             ModelState.AddModelError("Especialidad", "No es posible registrar duplicados de especialidad.");
             //return PartialView("AgregarPacientes"); redireccionar a donde corresponda
@@ -44,39 +46,40 @@ public class HomeController : Controller
 
 
             foreach (var especialidad in ListaEspecialidad)
-    {
-        Console.WriteLine($"ID: {especialidad.IdEspecialidad}, Nombre: {especialidad.Nombre}, Duración: {especialidad.Duracion}");
-    }
+            {
+                Console.WriteLine($"ID: {especialidad.IdEspecialidad}, Nombre: {especialidad.Nombre}, Duración: {especialidad.Duracion}");
+            }
             return RedirectToAction("AgregarEspecialidad");
         }
         ModelState.AddModelError("", "Formato de duración no válido.");
-         return RedirectToAction("Sala");
+        return RedirectToAction("Sala");
     }
 
-    [HttpPost]  
+    [HttpPost]
     public IActionResult AgregarPacientes(string nombre, string apellido, int cedula)
-    {   if (ListaPaciente.Any(p => p.Cedula == cedula))
+    {
+        if (ListaPaciente.Any(p => p.Cedula == cedula))
         {
             ModelState.AddModelError("Cedula", "La cédula ya está registrada.");
             //return PartialView("AgregarPacientes"); redireccionar a donde corresponda
         }
-        var nuevoPaciente = new Paciente(nombre, apellido,cedula); 
-        ListaPaciente.Add(nuevoPaciente);  
+        var nuevoPaciente = new Paciente(nombre, apellido, cedula);
+        ListaPaciente.Add(nuevoPaciente);
         Console.WriteLine("Nueva PACIENTE agregada:");
-             foreach (var paciente in ListaPaciente)
+        foreach (var paciente in ListaPaciente)
         {
             Console.WriteLine($"ID: {paciente.IdPaciente}, Nombre: {paciente.Nombre}, Apellido: {paciente.Apellido},cedula: {paciente.Cedula}");
         }
-             return RedirectToAction("Sala");
-        }
-        [HttpPost]
-        public IActionResult CrearConsultorio(string nuevoPaciente)
-        {
-            return PartialView("CrearConsultorio");
-        }
+        return RedirectToAction("Sala");
+    }
+    [HttpPost]
+    public IActionResult CrearConsultorio(string nuevoPaciente)
+    {
+        return PartialView("CrearConsultorio");
+    }
     // metodos get formularios 
 
-  
+
     public IActionResult AgregarPacientes()
     {
         return PartialView("AgregarPacientes");
@@ -85,17 +88,12 @@ public class HomeController : Controller
     {
         return PartialView("AgregarEspecialidad");
     }
-    public IActionResult CrearConsultorio()
-    {
-        return PartialView("CrearConsultorio");
-    }
-
     public async Task CargarPaciente()
     {
         string ruta = Path.Combine(Directory.GetCurrentDirectory(), "ArchivosJSON", "Pacientes.json");
         using FileStream leer = System.IO.File.OpenRead(ruta);
         ListaPaciente = await JsonSerializer.DeserializeAsync<List<Paciente>>(leer);
-            foreach (var paciente in ListaPaciente)
+        foreach (var paciente in ListaPaciente)
         {
             Console.WriteLine($"ID: {paciente.IdPaciente}, Nombre: {paciente.Nombre}, Apellido: {paciente.Apellido}");
         }
@@ -106,11 +104,29 @@ public class HomeController : Controller
         string ruta = Path.Combine(Directory.GetCurrentDirectory(), "ArchivosJSON", "Especialidades.json");
         using FileStream leer = System.IO.File.OpenRead(ruta);
         ListaEspecialidad = await JsonSerializer.DeserializeAsync<List<Especialidad>>(leer);
-          foreach (var especialidad in ListaEspecialidad)
+        foreach (var especialidad in ListaEspecialidad)
+        {
+            Console.WriteLine($"ID: {especialidad.IdEspecialidad}, Nombre: {especialidad.Nombre}, Duración: {especialidad.Duracion}");
+        }
+    }
+
+ public IActionResult CrearConsultorio()
     {
-        Console.WriteLine($"ID: {especialidad.IdEspecialidad}, Nombre: {especialidad.Nombre}, Duración: {especialidad.Duracion}");
-    }
-    }
+        if (ListaConsultorios.Count >= 15)
+        {
+            Console.WriteLine("LLeno");
+            TempData["Error"] = "La lista de consultorios ya esta llena";
+
+        }
+        else
+        {
+            Consultorios nueva = new Consultorios();
+            ListaConsultorios.Add(nueva);
+            TempData["Mensaje"] = "Se ha creado el consultorio exitosamente.";
+        }
+
+         return RedirectToAction("Sala");
+    }
 }
 
     
