b3e037f3f2f675b6eea71aa430249a91f56a7af6
ELDERPEREZ <125334333+Elder19@users.noreply.github.com>
2025-06-01 02:06:15 -0600

    conexion a interfaz

--- PY_Analisis/Controllers/HomeController.cs
+++ PY_Analisis/Controllers/HomeController.cs
@@ -10,9 +10,9 @@ namespace PY_Analisis.Controllers;
 
 public class HomeController : Controller
 {
-    public List<Paciente> ListaPaciente { get; set; } = new List<Paciente>();
-    public List<Especialidad> ListaEspecialidad { get; set; } = new List<Especialidad>();  
-    
+    public static List<Paciente> ListaPaciente { get; set; } = new List<Paciente>();
+    public static List<Especialidad> ListaEspecialidad { get; set; } = new List<Especialidad>();  
+    private static bool datosCargados = false;
       public IActionResult Index()
     {
         return View();
@@ -20,17 +20,42 @@ public class HomeController : Controller
 
     public async Task<IActionResult> Sala()
     {
+       if (!datosCargados)
+    {
         await CargarPaciente();
         await CargarEspecialidad();
+        datosCargados = true;
+    }
         return View(ListaPaciente);
     }
+      [HttpPost]
+   [HttpPost]
+    public IActionResult AgregarEspecialidad(string nombre, string duracion)
+    {
+        if (TimeOnly.TryParse(duracion, out TimeOnly duracionTime))
+        {
+            var nuevaEspecialidad = new Especialidad(nombre, duracionTime);
+            ListaEspecialidad.Add(nuevaEspecialidad);
+            Console.WriteLine("Nueva especialidad agregada:");
+
+
+            foreach (var especialidad in ListaEspecialidad)
+    {
+        Console.WriteLine($"ID: {especialidad.IdEspecialidad}, Nombre: {especialidad.Nombre}, Duración: {especialidad.Duracion}");
+    }
+
+         
+            return RedirectToAction("AgregarEspecialidad");
+        }
+
+        ModelState.AddModelError("", "Formato de duración no válido.");
+        return View("AgregarPacientes");
+    }
+
 
     // metodos get formularios 
 
-    public IActionResult CrearEspecialidad()
-    {
-        return View();
-    }
+  
     public IActionResult AgregarPacientes()
     {
         return PartialView("AgregarPacientes");
@@ -49,13 +74,24 @@ public class HomeController : Controller
         string ruta = Path.Combine(Directory.GetCurrentDirectory(), "ArchivosJSON", "Pacientes.json");
         using FileStream leer = System.IO.File.OpenRead(ruta);
         ListaPaciente = await JsonSerializer.DeserializeAsync<List<Paciente>>(leer);
-
+            foreach (var paciente in ListaPaciente)
+        {
+            Console.WriteLine($"ID: {paciente.IdPaciente}, Nombre: {paciente.Nombre}, Apellido: {paciente.Apellido}");
+        }
     }
-     public async Task CargarEspecialidad()
+    public async Task CargarEspecialidad()
     {
+
         string ruta = Path.Combine(Directory.GetCurrentDirectory(), "ArchivosJSON", "Especialidades.json");
         using FileStream leer = System.IO.File.OpenRead(ruta);
         ListaEspecialidad = await JsonSerializer.DeserializeAsync<List<Especialidad>>(leer);
-    }
-}  
+          foreach (var especialidad in ListaEspecialidad)
+    {
+        Console.WriteLine($"ID: {especialidad.IdEspecialidad}, Nombre: {especialidad.Nombre}, Duración: {especialidad.Duracion}");
+    }
+    }
+}
+
+    
+
 
